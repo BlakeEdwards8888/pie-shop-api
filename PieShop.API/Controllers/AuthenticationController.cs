98cfa581dba6e8ee3d040e002815135fd122d26a
blake-edwards <blake.edwards8888@gmail.com>
2026-03-29 11:15:24 -0400

    started added an authentication controller to simplify the authentication process

---
+++ PieShop.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace PieShop.API.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        IConfiguration configuration;
+
+        public class AuthenticationRequestBody
+        {
+            public string? UserName { get; set; }
+            public string? Password { get; set; }
+        }
+
+        class PieShopUser
+        {
+            public int UserId { get; set; }
+            public string UserName { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Role { get; set; }
+
+            public PieShopUser(int userId, string userName, string firstName, string lastName, string role)
+            {
+                UserId = userId;
+                UserName = userName;
+                FirstName = firstName;
+                LastName = lastName;
+                Role = role;
+            }
+        }
+
+        public AuthenticationController(IConfiguration configuration)
+        {
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        [HttpPost("authenticate")]
+        public ActionResult<string> Authenticate(AuthenticationRequestBody requestBody)
+        {
+            var user = ValidateCredentials(requestBody.UserName, requestBody.Password);
+
+            if (user == null) return Unauthorized();
+
+            var securityKey = new SymmetricSecurityKey(Convert.FromBase64String(configuration["Authentication:SecretForKey"]));
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claimsForToken = new List<Claim>
+            {
+                new Claim("customrole", user.Role)
+            };
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                configuration["Authentication:Issuer"],
+                configuration["Authentication:Audience"]);
+
+            return Ok();
+        }
+
+        private PieShopUser ValidateCredentials(string? userName, string? password)
+        {
+            return new PieShopUser(1, userName ?? "", "Blake", "Edwards", "admin");
+        }
+    }
+}
